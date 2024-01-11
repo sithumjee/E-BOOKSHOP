7b4a5e8c2cc8efefe1e64c9ae742ec881b767e6c
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-11 18:46:15 +0530

    popular

---
+++ frontend/src/Components/Assets/PopularBooks.js
@@ -0,0 +1,34 @@
+import image_01 from "../Assets/aboutImage.jpg";
+
+let popular_books = [
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: image_01,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: image_01,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: image_01,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: image_01,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+];
+
+export default popular_books;
--- frontend/src/Components/Home/Popular/Popular.jsx
+++ frontend/src/Components/Home/Popular/Popular.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Popular.css";
+import Item from "../../Items/Item";
+import popular_books from "../../Assets/PopularBooks";
 
 const Popular = () => {
   return (
     <div className="popular">
       <h1>POPULAR BOOKS</h1>
+      <div className="popular-item">
+        {popular_books.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
