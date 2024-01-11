ced77b426e0b7331b04dc0de43d97e48f3e7aafd
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-11 11:24:00 +0530

    about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import AboutUs from "./Pages/AboutUs";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ frontend/src/Pages/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./CSS/AboutUs.css";
+import aboutImage from "../Components/Assets/aboutImage.jpg";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutUs">
+      <h1>ORIGIN</h1>
+      <div className="top-container">
+        <div className="left-container">
+          <img src={aboutImage} alt="" />
+        </div>
+        <div className="right-container">
+          <p>
+            we aremldfffffffffffffffffffffffffffffffffffdffffffffffdddf dfjkl
+            dfj sfdddddddddddd dfdfmd, sdfdfdf s;elfefi dfjkl dfj sfdddddddddddd
+            dfdfmd, sdfdfdf s;elfefi dfjkl dfj sfdddddddddddd dfdfmd, sdfdfdf
+            s;elfefi dfjkl dfj sfdddddddddddd dfdfmd, sdfdfdf s;elfefi dfjkl dfj
+            sfdddddddddddd dfdfmd, sdfdfdf s;elfefi dfjkl dfj sfdddddddddddd
+            dfdfmd, sdfdfdf s;elfefi
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
