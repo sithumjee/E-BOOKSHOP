1a50d5378fb623260b1ee9297cf9b9ea59721e18
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-21 23:07:41 +0530

    products

--- frontend/src/Components/ProductDispaly/ProductDisplay.jsx
+++ frontend/src/Components/ProductDispaly/ProductDisplay.jsx
@@ -1,3 +1,4 @@
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 
 const ProductDisplay = (props) => {
@@ -6,19 +7,40 @@ const ProductDisplay = (props) => {
   return (
     <div className="productDisplay">
       <div className="display-left">
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
-        <h1>correct</h1>
+        <div className="productDisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
 
         <div className="productDisplay-img">
-          {/* <img className="productDisplay-img-main" src={product.image} alt="" /> */}
+          <img className="productDisplay-img-main" src={product.image} alt="" />
         </div>
+      </div>
+
+      <div className="display-right">
+        <h1>{product.name}</h1>
+        <div className="display-right-star">
+          <p>(122)</p>
+        </div>
+
+        <div className="display-right-price">
+          <div className="rightPriceOld">Rs {product.old_price}</div>
+          <div className="rightPriceNew">Rs {product.new_price}</div>
+        </div>
+
+        <div className="display-right-description">
+          <p>{product.description}</p>
+        </div>
+
+        <button>ADD TO CART</button>
+        <p className="displayCategory">
+          <span>Category :</span>Women , T-shirt, Crop Top{" "}
+        </p>
+        <p className="displayCategory">
+          <span>Tags :</span>Modern , Latest{" "}
+        </p>
       </div>
     </div>
   );
