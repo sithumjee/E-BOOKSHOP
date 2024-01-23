66d80e4035cd8a6a229fc1af0f5d0419c380ec38
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-23 23:44:39 +0530

    authors

---
+++ frontend/src/Components/Authors/AuthorList.js
@@ -0,0 +1,64 @@
+let authorList = [
+  {
+    id: 1,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 2,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 3,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 4,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 5,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 6,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 7,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 8,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 9,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+  {
+    id: 10,
+    name: "Sithum Jeevantha",
+    amount: 12,
+    path: "/author/sithumJeevantha",
+  },
+];
+
+export default authorList;
--- frontend/src/Components/Authors/Authors.jsx
+++ frontend/src/Components/Authors/Authors.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Authors.css";
+import authorList from "./AuthorList";
+import AuthorItem from "./AuthorItem/AuthorItem";
 
 const Authors = () => {
   return (
@@ -7,6 +9,19 @@ const Authors = () => {
       <div className="top-container">
         <input type="text" placeholder="Search by name" />
         <button>Search</button>
+      </div>
+
+      <div className="author-list">
+        {authorList.map((item, i) => {
+          return (
+            <AuthorItem
+              key={i}
+              id={item.id}
+              name={item.name}
+              amount={item.amount}
+            />
+          );
+        })}
       </div>
     </div>
   );
