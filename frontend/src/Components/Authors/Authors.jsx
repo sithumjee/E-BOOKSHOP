e5b05332e09cfc005a5c923648155aea1ae4f2a8
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-23 23:25:24 +0530

    authors

--- frontend/src/Components/Authors/Authors.jsx
+++ frontend/src/Components/Authors/Authors.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Authors.css";
 
 const Authors = () => {
-  return <div className="authors"></div>;
+  return (
+    <div className="authors">
+      <div className="top-container">
+        <input type="text" placeholder="Search by name" />
+        <button>Search</button>
+      </div>
+    </div>
+  );
 };
 
 export default Authors;
