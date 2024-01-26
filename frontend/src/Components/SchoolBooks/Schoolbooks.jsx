a41fec8929da7ade674d4e23ef660a58ae8b54e5
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-26 20:02:47 +0530

    school books

--- frontend/src/Components/SchoolBooks/Schoolbooks.jsx
+++ frontend/src/Components/SchoolBooks/Schoolbooks.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-// import "./Schoolbooks.css";
+import "./Schoolbooks.css";
 
 const Schoolbooks = () => {
-  return <div className="schoolbooks"></div>;
+  return (
+    <div className="schoolbooks">
+      <h1>--- Schoolbooks ---</h1>
+
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+        <button>Search</button>
+      </div>
+    </div>
+  );
 };
 
 export default Schoolbooks;
