ec977d731cc8528eeec608635f3678f7a174032e
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-30 19:46:49 +0530

    update contact us file

--- frontend/src/Pages/ContactUs.jsx
+++ frontend/src/Pages/ContactUs.jsx
@@ -4,93 +4,95 @@ import { Link } from "react-router-dom";
 
 const ContactUs = () => {
   return (
-    <div className="contactus">
-      <div className="main">
-        <h1>
-          BANDARAWELA (<span>Head Quatres</span>)
-        </h1>
-        <p>
-          <span>Address :</span>
-          No. 123, Main Street, Bandarawela.
-        </p>
+    <div className="main">
+      <div className="contactus">
+        <div className="main">
+          <h1>
+            BANDARAWELA (<span>Head Quatres</span>)
+          </h1>
+          <p>
+            <span>Address :</span>
+            No. 123, Main Street, Bandarawela.
+          </p>
 
-        <div className="mainDetails">
-          <div>
+          <div className="mainDetails">
+            <div>
+              <p>
+                <span>contact :</span>
+                <p>057-5682925</p>
+                <p>071-1663875</p>
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <span>Email: </span>
+                <Link>nenapiyasa@gmail.com</Link>
+              </p>
+              <p>
+                Follow us on Facebook
+                <Link> nenapiyasa</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="sub">
+          <div className="subbranches">
+            <h3>Colombo</h3>
             <p>
-              <span>contact :</span>
-              <p>057-5682925</p>
-              <p>071-1663875</p>
+              <span>call us</span>
+              057-5647383
+            </p>
+            <p>
+              <span>Whatsapp </span>0711663876
             </p>
           </div>
-
-          <div>
+          <div className="subbranches">
+            <h3>Kandy</h3>
             <p>
-              <span>Email: </span>
-              <Link>nenapiyasa@gmail.com</Link>
+              <span>call us</span>
+              057-5647383
             </p>
             <p>
-              Follow us on Facebook
-              <Link> nenapiyasa</Link>
+              <span>Whatsapp </span>0711663876
+            </p>
+          </div>
+          <div className="subbranches">
+            <h3>Matara</h3>
+            <p>
+              <span>call us</span>
+              057-5647383
+            </p>
+            <p>
+              <span>Whatsapp </span>0711663876
+            </p>
+          </div>
+          <div className="subbranches">
+            <h3>Anuradapura</h3>
+            <p>
+              <span>call us</span>
+              057-5647383
+            </p>
+            <p>
+              <span>Whatsapp </span>0711663876
             </p>
           </div>
         </div>
-      </div>
 
-      <hr />
+        <hr />
 
-      <div className="sub">
-        <div className="subbranches">
-          <h3>Colombo</h3>
-          <p>
-            <span>call us</span>
-            057-5647383
-          </p>
-          <p>
-            <span>Whatsapp </span>0711663876
-          </p>
-        </div>
-        <div className="subbranches">
-          <h3>Kandy</h3>
-          <p>
-            <span>call us</span>
-            057-5647383
-          </p>
-          <p>
-            <span>Whatsapp </span>0711663876
-          </p>
-        </div>
-        <div className="subbranches">
-          <h3>Matara</h3>
-          <p>
-            <span>call us</span>
-            057-5647383
-          </p>
-          <p>
-            <span>Whatsapp </span>0711663876
-          </p>
-        </div>
-        <div className="subbranches">
-          <h3>Anuradapura</h3>
-          <p>
-            <span>call us</span>
-            057-5647383
-          </p>
-          <p>
-            <span>Whatsapp </span>0711663876
-          </p>
-        </div>
-      </div>
-
-      <hr />
-
-      <div className="feedback">
-        <div className="feedback-banner">
-          <div>
-            <p>Send your feedback to us</p>
-          </div>
-          <div>
-            <input type="text" />
-            <button>submit</button>
+        <div className="feedback">
+          <div className="feedback-banner">
+            <div>
+              <p>Send your feedback to us</p>
+            </div>
+            <div>
+              <input type="text" />
+              <button>submit</button>
+            </div>
           </div>
         </div>
       </div>
