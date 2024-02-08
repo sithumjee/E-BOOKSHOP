177319175cb524fc1418e7786b8bb29a24cf031b
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-02-09 00:22:07 +0530

    mm

--- frontend/src/Components/PastPapers/PastPapers.jsx
+++ frontend/src/Components/PastPapers/PastPapers.jsx
@@ -5,7 +5,7 @@ const PastPapers = () => {
   return (
     <div className="pastPapers">
       <h1>Past Papers</h1>
-      <h3>Download and review all the pastPapers from 2000</h3>
+     
     </div>
   );
 };
