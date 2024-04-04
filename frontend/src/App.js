41e82208a6722931c4ac58a68f5f0e9f78e48af3
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-04-04 14:15:00 +0530

    auth correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ import PastPapers from "./Components/PastPapers/PastPapers";
 import ShortcutLinks from "./Components/Navbar/Sortcut_Links/SOrtcutLinks";
 import BookSearch from "./Components/SearchBar/BookSearch";
 import Cart from "./Components/Cart/Cart";
-import AuthPage from "./Pages/AuthPage";
 
 function App() {
   return (
