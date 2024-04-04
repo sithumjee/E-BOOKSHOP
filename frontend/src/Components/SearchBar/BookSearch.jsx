ba93b36a829d4b3f3e75c215521e29234beef02b
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-04-04 13:07:24 +0530

    search bar

--- frontend/src/Components/SearchBar/BookSearch.jsx
+++ frontend/src/Components/SearchBar/BookSearch.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./BookSearch.css";
 
-const BookSearch = () => {
-  return <div></div>;
+const BookSearch = ({ allProducts }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = () => {
+    // Perform the search based on the search term
+    const searchResults = allProducts.filter((product) => {
+      return (
+        product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.Author_Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.category.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+
+    // Handle the search results, e.g., display them or pass them to another component
+    console.log(searchResults);
+  };
+
+  return (
+    <div className="searchbox">
+      <input
+        type="text"
+        placeholder="Search for books"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 };
 
 export default BookSearch;
